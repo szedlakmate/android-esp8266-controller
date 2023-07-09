deb2866dd4af70e0e18e770d6b229c4441d248d9
Szedlák Máté <szedlakmate@gmail.com>
2023-07-09 21:35:43 +0100

    Send POST request

--- app/src/main/java/com/mateszedlak/ledcontroller/FirstFragment.kt
+++ app/src/main/java/com/mateszedlak/ledcontroller/FirstFragment.kt
@@ -2,54 +2,94 @@ package com.mateszedlak.ledcontroller
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.mateszedlak.ledcontroller.databinding.FragmentFirstBinding
-import java.util.logging.Logger
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.DataOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
+/** A simple [Fragment] subclass as the default destination in the navigation. */
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+  private var _binding: FragmentFirstBinding? = null
+  private val HOST = "http://192.168.50.18:3000"
+  val url = URL("$HOST/blink")
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+  // This property is only valid between onCreateView and
+  // onDestroyView.
+  private val binding
+    get() = _binding!!
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+  override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?
+  ): View {
+    _binding = FragmentFirstBinding.inflate(inflater, container, false)
+    return binding.root
+  }
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
 
+    binding.buttonOn.setOnClickListener {
+      Log.i(TAG, "Button ON clicked")
+      try {
+        sendRequest("true")
+      } catch (e: Exception) {
+        Log.w(TAG, "Error: ${e.message}")
+      }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonOn.setOnClickListener {
-            Log.i(TAG, "Button ON clicked")
-
-        }
-
-        binding.buttonOff.setOnClickListener {
-            Log.i(TAG,"Button OFF clicked")
-        }
+    binding.buttonOff.setOnClickListener {
+      Log.i(TAG, "Button OFF clicked")
+      try {
+        sendRequest("false")
+      } catch (e: Exception) {
+        Log.w(TAG, "Error: ${e.message}")
+      }
     }
+  }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+  @OptIn(DelicateCoroutinesApi::class)
+  private fun sendRequest(data: String) {
+    GlobalScope.launch(Dispatchers.IO) {
+      var responseCode = -1
+      try {
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "POST"
+        connection.setRequestProperty("Content-Type", "text/plain")
+        connection.doOutput = true
 
-    companion object {
-        private const val TAG = "FirstFragment"
+        val outputStream = DataOutputStream(connection.outputStream)
+        outputStream.writeBytes(data)
+        outputStream.flush()
+        outputStream.close()
+
+        responseCode = connection.responseCode
+      } catch (e: Exception) {
+        Log.w(TAG, "Error: ${e.message}")
+        e.printStackTrace()
+      }
+
+      withContext(Dispatchers.Main) { Log.d(TAG, "Response code: $responseCode") }
     }
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
+
+  companion object {
+    private const val TAG = "FirstFragment"
+  }
 }
